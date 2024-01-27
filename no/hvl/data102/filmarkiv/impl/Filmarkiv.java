ee60a2f7e5e29cd2b674eb4a1b9d841f93c88619
h598904 <143750480+h598904@users.noreply.github.com>
2024-01-27 14:10:02 +0100

    ny filmarkiv endringer

--- no/hvl/data102/filmarkiv/impl/Filmarkiv.java
+++ no/hvl/data102/filmarkiv/impl/Filmarkiv.java
@@ -1,5 +1,7 @@
 package no.hvl.data102.filmarkiv.impl;
 
+import no.hvl.data102.filmarkiv.adt.FilmarkivADT;
+
 public class Filmarkiv implements FilmarkivADT {
     private Film[] films;
     private int count;
@@ -7,15 +9,19 @@ public class Filmarkiv implements FilmarkivADT {
     public Filmarkiv(int capacity) {
         films = new Film[capacity];
         count = 0;
-            }
+    }
+
+    @Override
     public Film finnFilm(int nr) {
         for (int i = 0; i < count; i++) {
-            if (films[i].getNummer() == nr) { // Assuming Film has getNummer() method
+            if (films[i].getNummer() == nr) {
                 return films[i];
             }
         }
         return null;
     }
+
+    @Override
     public void leggTilFilm(Film nyFilm) {
         if (count == films.length) {
             expand();
@@ -23,6 +29,14 @@ public class Filmarkiv implements FilmarkivADT {
         films[count] = nyFilm;
         count++;
     }
+
+    private void expand() {
+        Film[] newFilms = new Film[films.length * 2];
+        System.arraycopy(films, 0, newFilms, 0, films.length);
+        films = newFilms;
+    }
+
+    @Override
     public boolean slettFilm(int filmnr) {
         for (int i = 0; i < count; i++) {
             if (films[i].getNummer() == filmnr) {
@@ -33,26 +47,32 @@ public class Filmarkiv implements FilmarkivADT {
         }
         return false;
     }
+
+    @Override
     public Film[] soekTittel(String delstreng) {
-        Film[] result = new Film[count];
+        Film[] temp = new Film[films.length];
         int found = 0;
         for (int i = 0; i < count; i++) {
             if (films[i].getTittel().contains(delstreng)) {
-                result[found++] = films[i];
+                temp[found++] = films[i];
             }
         }
-        return trimArray(result);
+        return trimTab(temp, found);
     }
+
+    @Override
     public Film[] soekProdusent(String delstreng) {
-        Film[] result = new Film[count];
+        Film[] temp = new Film[films.length];
         int found = 0;
         for (int i = 0; i < count; i++) {
             if (films[i].getProdusent().contains(delstreng)) {
-                result[found++] = films[i];
+                temp[found++] = films[i];
             }
         }
-        return trimArray(result);
+        return trimTab(temp, found);
     }
+
+    @Override
     public int antall(Sjanger sjanger) {
         int total = 0;
         for (int i = 0; i < count; i++) {
@@ -62,9 +82,17 @@ public class Filmarkiv implements FilmarkivADT {
         }
         return total;
     }
+
+    @Override
     public int antall() {
         return count;
     }
 
-
-
+    private Film[] trimTab(Film[] tab, int n) {
+        Film[] nytab = new Film[n];
+        for (int i = 0; i < n; i++) {
+            nytab[i] = tab[i];
+        }
+        return nytab;
+    }
+}
